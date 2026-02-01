c0ca2dbabcde26e52b80b5e9792efc20ce11c852
Keshia Poedjiono <keshiapud@gmail.com>
2026-02-01 23:28:36 +0800

    add routing using boardId

---
+++ src/app/api/boards/[boardId]/route.ts
@@ -0,0 +1,44 @@
+import { prisma } from "@/lib/prisma";
+import { requireAuth } from "@/lib/auth";
+import { NextRequest } from "next/server";
+
+
+export async function GET (
+    request: NextRequest,
+    { params }: { params: { boardId: string } },
+){
+    try {
+        const { boardId } = params;
+        const userId = requireAuth(request);
+
+        const board = await prisma.board.findFirst({
+            where: { 
+                id: boardId,
+                ownerId: userId,
+            }
+        })
+
+        if (!board) {
+            return new Response(
+            JSON.stringify({ error: "Board not found" }),
+            { status: 404 }
+            );
+        }
+
+        return new Response(JSON.stringify({ 
+            board 
+        }),
+        {
+            status: 200,
+            headers: { 'Content-Type': 'application/json' },
+        })
+    }
+    catch (error){
+        return new Response(
+            JSON.stringify({ error: "Unauthorized" }),
+            { status: 404 }
+        )
+    }
+};
+
+
--- src/app/api/boards/route.ts
+++ src/app/api/boards/route.ts
@@ -46,14 +46,3 @@ export async function POST(request: Request){
         )
     }
 }
-
-// -- CreateTable
-// CREATE TABLE "Board" (
-//     "id" TEXT NOT NULL,
-//     "title" TEXT NOT NULL,
-//     "ownerId" TEXT NOT NULL,
-//     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-//     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-//     CONSTRAINT "Board_pkey" PRIMARY KEY ("id")
-// );
